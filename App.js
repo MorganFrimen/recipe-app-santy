17a9cbe761f739be9d2c04d36748edd4c8ac3954
Sergey Tsarev <mr.MorganFrimen@yandex.ru>
2022-04-28 11:07:10 +0300

    start

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return(
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const Saved = () => {
+  return(
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <>
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={({route}) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName 
+            if (route.name == 'Home') {
+              iconName = 'md-home'
+              
+            } else if (route.name == 'Saved') {
+              iconName = 'md-heart';
+            }
+            return<Ionicons name={iconName}  size={size} color={color} />
+          },
+          tabBarActiveTintColor: '#FC5252',
+          tabBarInactiveTintColor: '#898989',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Tab.Screen name="Saved" component={Saved} options={{headerShown: false}} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    <StatusBar style='auto' />
+  </>
+    
+
   );
 }
 
